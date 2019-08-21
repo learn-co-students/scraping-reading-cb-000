81063f57a3c10117d0ba30afb93a18f34c3f4c0c
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 10:40:26 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
+# puts doc.css(".headline-26OIBN").text.strip
 
+courses = doc.css(".title-oE5vT4")
 
+courses.each do |course|
+  puts course.text.strip
+end
